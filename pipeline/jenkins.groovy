93e5969b9ccefd5f81f2502368e76f75b2f8c70e
Ruslan Yakovlev <yakovlev.ruslan@gmail.com>
2025-12-03 17:07:51 +0000

    new file:   pipeline/jenkins.groovy

---
+++ pipeline/jenkins.groovy
@@ -0,0 +1,27 @@
+
+pipeline {
+    agent any
+    
+    parameters {
+        choice(
+            name: 'OS',
+            choices: ['linux', 'darwin', 'windows'],
+            description: 'Target operating system'
+        )
+        choice(
+            name: 'ARCH',
+            choices: ['amd64', 'arm64'],
+            description: 'Target architecture'
+        )
+        booleanParam(
+            name: 'SKIP_TESTS',
+            defaultValue: false,
+            description: 'Skip running tests'
+        )
+        booleanParam(
+            name: 'SKIP_LINT',
+            defaultValue: false,
+            description: 'Skip running linter'
+        )
+    }
+}
